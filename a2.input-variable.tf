07d4d2b83e14a15b2b272f215074a91ea5679749
gopal <gopal1409@gmail.com>
2022-12-07 08:31:07 +0530

    vnet added

--- a2.input-variable.tf
+++ a2.input-variable.tf
@@ -4,8 +4,10 @@ variable "business_devision" {
   description = "Business devision in large organization"
   type = string
   default = "hr"
+  ##every varaible can accept single argument
 }
-
+#when my resourcegrp created 
+#hr-dev-environment-location 
 #environment variable 
 variable "environment" {
   type = string
