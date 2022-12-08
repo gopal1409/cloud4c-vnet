9d70c84f73b82519a76226f7135e32d09bd937a0
gopal <gopal1409@gmail.com>
2022-12-08 09:02:33 +0530

    terraform with double subnet

--- a10.app-subnet-nsg.tf
+++ a10.app-subnet-nsg.tf
@@ -1,42 +1,41 @@
 
 #create websubnet
-resource "azurerm_subnet" "websubnet" {
-  name = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_name}"
+resource "azurerm_subnet" "appsubnet" {
+  name = "${azurerm_virtual_network.vnet.name}-${var.app_subnet_name}"
   resource_group_name = azurerm_resource_group.rg.name 
   virtual_network_name = azurerm_virtual_network.vnet.name 
-  address_prefixes = var.web_subnet_address
+  address_prefixes = var.app_subnet_address
 }
 
 #create nsg
-resource "azurerm_network_security_group" "web_subnet_nsg" {
+resource "azurerm_network_security_group" "app_subnet_nsg" {
     #security group name is combination of websubnet name + nsg
-  name = "${azurerm_subnet.websubnet.name}-nsg"
+  name = "${azurerm_subnet.appsubnet.name}-nsg"
   resource_group_name = azurerm_resource_group.rg.name 
   location            = azurerm_resource_group.rg.location
 }
 
 #assocaite the nsg with subnet
-resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_associate" {
+resource "azurerm_subnet_network_security_group_association" "app_subnet_nsg_associate" {
   depends_on = [
-    azurerm_network_security_rule.web_nsg_rule_inbound
+    azurerm_network_security_rule.app_nsg_rule_inbound
   ]
   #it will create an explicit dependency on those resources
   #it will increase the time to create the resource. 
   #every nsg rule you have created will be disassociate from nsg and it will re associate. 
-   subnet_id                 = azurerm_subnet.websubnet.id
-   network_security_group_id = azurerm_network_security_group.web_subnet_nsg.id
+   subnet_id                 = azurerm_subnet.appsubnet.id
+   network_security_group_id = azurerm_network_security_group.app_subnet_nsg.id
 }
 
 locals {
-  web_inbound_port = {
-    "100" : "80",
-    "110" : "443",
-    "120" : "22" #in terraform if your key start with numberic agains the key the value you are going to put : value
+  app_inbound_port = {
+    "110" : "3389",
+    "120" : "3306" #in terraform if your key start with numberic agains the key the value you are going to put : value
   }
 }
 #nsg inbound rule for webtier subnet
-resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
-  for_each = local.web_inbound_port
+resource "azurerm_network_security_rule" "app_nsg_rule_inbound" {
+  for_each = local.app_inbound_port
     name                       = "Rule-Port-${each.value}"
     priority                   = each.key
     direction                  = "Inbound"
@@ -44,10 +43,10 @@ resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = each.value
-    source_address_prefix      = "*"
+    source_address_prefix      = "12.12.12.0/24"
     destination_address_prefix = "*"
     resource_group_name = azurerm_resource_group.rg.name 
-    network_security_group_name = azurerm_network_security_group.web_subnet_nsg.name 
+    network_security_group_name = azurerm_network_security_group.app_subnet_nsg.name 
   }
 
 
--- a6.vnet-input-variable.tf
+++ a6.vnet-input-variable.tf
@@ -19,3 +19,13 @@ variable "web_subnet_address" {
   type = list(string)
   default = [ "10.0.1.0/24" ]
 }
+
+variable "app_subnet_name" {
+  type = string 
+default = "appsubnet"
+}
+
+variable "app_subnet_address" {
+  type = list(string)
+  default = [ "10.0.2.0/24" ]
+}
