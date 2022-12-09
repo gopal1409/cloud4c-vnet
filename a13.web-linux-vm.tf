3584daa240e30155d073d6750b936d71b3310eb5
gopal <gopal1409@gmail.com>
2022-12-09 08:23:52 +0530

    replace the custom data

--- a13.web-linux-vm.tf
+++ a13.web-linux-vm.tf
@@ -1,19 +1,18 @@
-###local block for bootstrap data
 locals {
-  web_vm_custom_data = <<CUSTOM_DATA
+webvm_custom_data = <<CUSTOM_DATA
 #!/bin/sh
-sudo yum update -y
+#sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo systemctl start httpd  
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
-sudo chmod -R 777 /var/www/html
-sudo echo "Welcome to stock - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
+sudo chmod -R 777 /var/www/html 
+sudo echo "Welcome to cloud4 - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
 sudo mkdir /var/www/html/app1
-sudo echo "Welcome to stock - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
-sudo echo "Welcome to stock - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
-sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stock - WebVM APP-1 </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+sudo echo "Welcome to cloud4 - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
+sudo echo "Welcome to cloud4 - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
+sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to cloud4 - WebVM APP-1 </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
 sudo curl -H "Metadata:true" --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-09-01" -o /var/www/html/app1/metadata.html
 CUSTOM_DATA
 }
@@ -43,6 +42,6 @@ resource "azurerm_linux_virtual_machine" "web_linuxvm" {
     sku       = "83-gen2"
     version   = "latest"
   }
-  custom_data = base64encode(local.web_vm_custom_data)
+  custom_data = base64encode(local.webvm_custom_data)
   #custom_data = filebase64(${path.module}/app-scripts/redhat-vm-script.sh)
 }
