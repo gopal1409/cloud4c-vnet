44cb3efa301a8272e29f3534bf74bfefe058dfe8
gopal <gopal1409@gmail.com>
2022-12-12 08:49:45 +0530

    azure bastion host

--- a16.bastion-input-varaible.tf
+++ a16.bastion-input-varaible.tf
@@ -4,5 +4,5 @@ variable "bastion_service_subnet_name" {
 }
 
 variable "bastion_service_address_prefixes" {
-  default = ["10.0.101"]
+  default = ["10.0.101.0/27"]
 }
---
+++ a20.bastionip.tf
@@ -0,0 +1,4 @@
+output "bastion_host_ip" {
+  description = "Bastion Host Linux Public Ip"
+  value = azurerm_public_ip.bastion_linux_publicip.ip_address
+}
